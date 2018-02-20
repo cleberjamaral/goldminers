d35671f453da00f6e35fcafceced382e5b5bb4fe
clebercbr <cleberjamaral@gmail.com>
2018-02-20 16:15:36 -0300

    adding shell scripts for tests

---
+++ gold_miners.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/cleber/Projetos/goldminers
+java -classpath /home/cleber/jacamo-0.7/libs/ant-launcher-1.10.1.jar org.apache.tools.ant.launch.Launcher -e -f bin/gold_miners.xml run
+
