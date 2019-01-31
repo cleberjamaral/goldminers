48a44780239d388958e22e22422f328f21f203da
Cleber Jorge Amaral <cleberjamaral@gmail.com>
2019-01-31 08:40:48 -0200

    old scripts replaced by gradle

--- gold_miners.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/cleber/Projetos/goldminers
-java -classpath /home/cleber/jacamo-0.7/libs/ant-launcher-1.10.1.jar org.apache.tools.ant.launch.Launcher -e -f bin/gold_miners.xml run
-
--- joining_miner.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /home/pi/Projetos/goldminers
-#java -Dpi4j.linking=dynamic -classpath /homer/pi/jacamo-0.7/libs/ant-launcher-1.10.1.jar org.apache.tools.ant.launch.Launcher -e -f bin/joining_miner.xml run
-java -classpath /home/pi/jacamo-0.7/libs/ant-launcher-1.10.1.jar org.apache.tools.ant.launch.Launcher -e -f bin/joining_miner.xml run
